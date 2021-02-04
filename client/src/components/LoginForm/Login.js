1c362b0904b731280ba080e4d0c5e38894ec97ce
wilsoncollin7 <wilsoncollin7@gmail.com>
2021-02-04 16:28:39 -0500

    added basic logic to form

--- client/src/components/LoginForm/Login.js
+++ client/src/components/LoginForm/Login.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 
 function Login() {
+
+  const [formObject, setFormObject] = useState({});
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setFormObject({...formObject, [name]: value})
+  };
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (formObject.email && formObject.password) {
+      console.log(formObject);
+    }
+  }
+
   return(
     <form className="form">
           <div className="formInput">
-            <input type="email" value="Email"/>
+            <input onChange={handleInputChange} type="email" placeholder="Email" name="email"/>
             </div>
           <div className="formInput">
-            <input type="password" value="Password"/>
+            <input onChange={handleInputChange} type="password" placeholder="Password" name="password"/>
             </div>
-        <button>Submit</button>
+        <button onClick={handleFormSubmit} type="submit">Submit</button>
       </form>
   )
 }
--- client/src/components/LoginForm/Register.js
+++ client/src/components/LoginForm/Register.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 // styling
 import "./style.css";
 
 function Register() {
+
+  const [formObject, setFormObject] = useState({});
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setFormObject({...formObject, [name]: value})
+  };
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (formObject.firstName && formObject.lastName && formObject.email && formObject.password) {
+      console.log(formObject);
+    }
+  }
+
   return(
       <form className="form">
           <div className="formInput">
-            <input type="text" value="First Name"/>
+            <input onChange={handleInputChange} type="text" placeholder="First Name" name="firstName"/>
             </div>
           <div className="formInput">
-            <input type="text" value="Last Name"/>
+            <input onChange={handleInputChange} type="text" placeholder="Last Name" name="lastName"/>
             </div>
           <div className="formInput">
-            <input type="email" value="Email"/>
+            <input onChange={handleInputChange} type="email" placeholder="Email" name="email"/>
             </div>
           <div className="formInput">
-            <input type="password" value="Password"/>
+            <input onChange={handleInputChange} type="password" placeholder="Password" name="password"/>
             </div>
-        <button>Submit</button>
+        <button onClick={handleFormSubmit} type="submit">Register</button>
       </form>
   )
 }
