48ff3a8587749971af698d8e33b9d21c26c3208f
wilsoncollin7 <wilsoncollin7@gmail.com>
2021-02-08 09:29:14 -0500

    can register a user

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,7 +1,24 @@
+const db = require("../models");
+
 module.exports = {
   register: (req, res) => {
-    const { fisrtName, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
     console.log(req.body);
-    return res.json(req.body);
+    db.User.findOne({ 'email': email }, (err, userMatch) => {
+      if (userMatch) {
+        console.log(`User Already Exists at ${email}`);
+        return res.send("User Already Exists");
+      }
+      const newUser = new db.User({
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+        'password': password
+      });
+      newUser.save((err, savedUser) => {
+        if (err) return res.json(err);
+        return res.json(savedUser);
+      });
+    });
   }
 }
