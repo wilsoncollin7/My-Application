7c0b8f738319ef4bb0e004a4f316aa247ab82584
wilsoncollin7 <wilsoncollin7@gmail.com>
2021-02-16 11:27:00 -0500

    added conditional rendering to home

---
+++ client/src/components/HomeComponent/HomeComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// styling
+import "./style.css";
+// components
+import Container from "../Container/Container";
+
+function HomeComponent() {
+
+  return(
+    <Container>
+      <p>this is home</p>
+      <p>Adding some stuff here</p>
+    </Container>
+  )
+}
+
+export default HomeComponent;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
 // components
 import HeaderImage from "../components/HeadImage/HeadImage";
+import Card from "../components/Card/Card";
+import HomeComponent from "../components/HomeComponent/HomeComponent";
+
 
 function Home() {
   return(
     <>
-      <h1>HOME</h1>
       <HeaderImage />
+      {
+        localStorage.getItem("user") ?
+        <Card>
+          <HomeComponent />
+        </Card> : 
+        
+        <Card>
+          <h1>MUST LOG IN</h1>
+        </Card>
+      }
+      
     </>
   )
 }
